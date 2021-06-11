f88bfcc2a0b8a6dbd76afedcc5e7f5eb35785a8e
gauravt <gauravt@iprogrammer.co>
2021-06-11 15:45:48 +0530

    Project Started

---
+++ watchlist_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
+# Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {'movies': list(movies.values())}
+    return JsonResponse(data)
+
+
+def movie_details(request, pk):
+    movies = Movie.objects.get(pk= pk)
+    data = {
+        'name': movies.name,
+        'description': movies.description,
+        'active': movies.active
+    }
+
+    return JsonResponse(data)
+
+
